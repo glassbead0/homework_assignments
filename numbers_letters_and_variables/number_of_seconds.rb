4b1cc12b36f949ee10d3dcb3bb773beeacdf83b2
Aaron Glasenapp <glassbead@gmail.com>
2014-06-30 07:40:45 -0600

    improved number_of_seconds.rb

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -9,14 +9,21 @@ puts "There are #{seconds_in_minute} seconds in a minute"
 puts "There are #{minutes_in_hour} minutes in an hour"
 puts "There are #{hours_in_day} hours in a day"
 puts "There are #{days_in_week} days in a week"
-puts 'This means that there are:'
-puts "  #{seconds_in_minute * minutes_in_hour} seconds in an hour,"
-puts "  #{seconds_in_minute * minutes_in_hour * hours_in_day} seconds in a day,"
-puts "  #{seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week}
+
+seconds_in_hour = seconds_in_minute * minutes_in_hour
+seconds_in_day = seconds_in_hour * hours_in_day
+seconds_in_week = seconds_in_day * days_in_week
+
+puts 'That means there are:'
+puts "  #{seconds_in_hour} seconds in an hour,"
+puts "  #{seconds_in_day} seconds in a day,"
+puts "  #{seconds_in_week}
 seconds in a week"
 
-puts "That means when you turn 20, you've been alive for #{20 * 52 *
-  days_in_week * hours_in_day * minutes_in_hour * seconds_in_minute} seconds,"
+seconds_in_20_years = seconds_in_week * 52 * 20
+seconds_in_100_years = seconds_in_20_years * 5
+
+puts "That means when you turn 20, you've been alive for #{seconds_in_20_years} seconds,"
 puts "and if you make it to 100, you will have lived
-#{100 * 52 * days_in_week * hours_in_day * minutes_in_hour * seconds_in_minute}
+#{seconds_in_100_years}
 seconds. Make them count!"
