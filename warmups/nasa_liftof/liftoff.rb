e62421e70898ba2c6fc93d5ad89586b10bb378f1
Aaron Glasenapp <glassbead@gmail.com>
2014-07-02 19:42:58 -0600

    added alternate version of liftoff.rb

--- warmups/nasa_liftof/liftoff.rb
+++ warmups/nasa_liftof/liftoff.rb
@@ -5,3 +5,12 @@ puts 'T - 10'
   puts 10 - count
 end
 puts 'We have liftoff!'
+
+# alternate version
+
+# start_count_at = 10
+# puts "T - #{start_count_at}"
+# (start_count_at - 1).downto(1).each do |count|
+#   puts count
+# end
+# puts 'We have liftoff!'
