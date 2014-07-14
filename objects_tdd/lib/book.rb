7d19adb63f521011de9c64aa9a31759c472d5238
Aaron Glasenapp <glassbead@gmail.com>
2014-07-14 13:26:14 -0600

    completed book class using TDD

--- objects_tdd/lib/book.rb
+++ objects_tdd/lib/book.rb
@@ -1,18 +1,51 @@
 # book
 class Book
-  def initialize
+  def initialize(book_name, number_of_pages = 100)
     @current_page = 0
+    @number_of_pages = number_of_pages
+    @pages_already_read = []
+    @book_completed = false
+    @book_name = book_name
   end
 
-  def turn_to_page(page)
-    @current_page = page
+  def turn_to_page(page) # you can turn to any given page. does not mark pages as read
+    if page <= @number_of_pages && page >= 0
+      @current_page = page.floor
+    else
+      puts "This book doesn't have that many pages"
+      @current_page
+    end
   end
 
-  def turn_page
-    @current_page += 1
+  def book_read?
+    if @pages_already_read == (0..@number_of_pages).to_a
+      puts "Congradulations! You finished #{@book_name}!"
+      @book_completed = true
+    else
+      @book_completed = false
+    end
+  end
+
+  def turn_page # will turn the page and mark the previous one as read.
+    if @current_page < @number_of_pages
+      @pages_already_read += [@current_page]
+      @pages_already_read.sort!.uniq
+      book_read?
+      @current_page += 1
+    else
+      puts "You're at the end of the book."
+      @pages_already_read += [@current_page]
+      @pages_already_read.sort!.uniq
+      book_read?
+      @current_page
+    end
   end
 
   def current_page
     @current_page
+  end
+
+  def pages_already_read
+    @pages_already_read.sort!.uniq
   end
 end
--- objects_tdd/spec/lib/book_spec.rb
+++ objects_tdd/spec/lib/book_spec.rb
@@ -1,12 +1,78 @@
 require 'rspec'
 require './lib/book'
+
 describe Book do
 
-  it 'should keep track of pages' do
-    expect(subject.current_page).to eq(0)
-    subject.turn_to_page(50)
-    expect(subject.current_page).to eq(50)
-    subject.turn_page
-    expect(subject.current_page).to eq(51)
+  describe 'current_page' do
+    subject { Book.new('The Grapes of Math') }
+    it 'should keep track of pages' do
+      expect(subject.current_page).to eq(0)
+      subject.turn_to_page(50)
+      expect(subject.current_page).to eq(50)
+      subject.turn_page
+      expect(subject.current_page).to eq(51)
+    end
+  end
+
+  # keeps an array of pages already read. a page gets read when a single page is turned
+  describe 'pages_already_read' do
+    subject { Book.new('Euclid in the Rainforest') }
+    it 'should tell you what pages you\'ve read' do
+      expect(subject.pages_already_read).to eq([])
+      subject.turn_page
+      expect(subject.pages_already_read).to eq([0])
+      5.times do
+        subject.turn_page
+      end
+      expect(subject.pages_already_read).to eq([0, 1, 2, 3, 4, 5])
+      subject.turn_to_page(50)
+      10.times do
+        subject.turn_page
+      end
+      expect(subject.pages_already_read).to eq([0, 1, 2, 3, 4, 5, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59])
+      subject.turn_to_page(40)
+      15.times do
+        subject.turn_page
+      end
+      expect(subject.pages_already_read).to eq([0, 1, 2, 3, 4, 5, 40, 41, 42, 43, 44, 45, 46, 47,
+                                                48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59])
+    end
+  end
+
+  context 'errors with page numbers' do
+    subject { Book.new('Harry Potter') }
+    describe 'turn_to_page that is out of range' do
+      it 'should stay on current page' do
+        expect(subject.turn_to_page(150)).to eq(0)
+        expect(subject.turn_to_page(-5)).to eq(0)
+      end
+    end
+
+    describe 'turn_page at end of book' do
+      it 'should stay on last page' do
+        subject.turn_to_page(100)
+        subject.turn_page
+        expect(subject.current_page).to eq(100)
+      end
+    end
+
+    describe 'turn to a floating point page' do
+      it 'should round down' do
+        expect(subject.turn_to_page(5.3)).to eq(5)
+      end
+    end
+  end
+
+  describe 'finished book' do
+    subject { Book.new('Life Of Pi', 174) }
+    it 'should tell you when you finish reading the book' do
+      expect(subject.book_read?).to eq(false)
+      174.times do
+        subject.turn_page
+      end
+      expect(subject.book_read?).to eq(false)
+      subject.turn_page
+      expect(subject.book_read?).to eq(true)
+    end
   end
 end
